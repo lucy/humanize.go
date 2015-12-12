f3e40451fde9f94f12c965e94d54dd426de9e369
lucy <ne.tetewi@gmail.com>
2015-12-12 18:54:27 +0100

    speed up bytes

--- bytes.go
+++ bytes.go
@@ -1,6 +1,6 @@
 package humanize
 
-import "fmt"
+import "strconv"
 
 // IEC Sizes
 const (
@@ -12,11 +12,22 @@ const (
 	EiB = PiB * 1024
 )
 
+func itoa3(b []byte, x int) []byte {
+	if x/100 != 0 {
+		b = append(b, '0'+byte(x/100))
+	}
+	if x/100 != 0 || x/10%10 != 0 {
+		b = append(b, '0'+byte(x/10%10))
+	}
+	b = append(b, '0'+byte(x%10))
+	return b
+}
+
 // BytesM produces a human readable representation of an IEC size with custom
 // suffixes.
 func BytesM(s uint64, suf []string) string {
 	if s < 999 {
-		return fmt.Sprintf("%d%s", s, suf[0])
+		return string(append(strconv.AppendUint(nil, s, 10), suf[0]...))
 	}
 
 	m := 0
@@ -27,10 +38,15 @@ func BytesM(s uint64, suf []string) string {
 		u /= 1024
 		m++
 	}
-	if u < 10 {
-		return fmt.Sprintf("%0.1f%s", float64(s)/float64(div), suf[m])
+	v := float64(s) / div
+	b := make([]byte, 0, 3+len(suf[m]))
+	if v < 10 {
+		d := byte(v*10 + 0.5)
+		b = append(b, '0'+d/10, '.', '0'+d%10)
+	} else {
+		b = itoa3(b, int(v+0.5))
 	}
-	return fmt.Sprintf("%.0f%s", float64(s)/float64(div), suf[m])
+	return string(append(b, suf[m]...))
 }
 
 // Bytes produces a human readable representation of an IEC size.
